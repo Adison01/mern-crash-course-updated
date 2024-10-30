da485e8ffd7116bff6f9d60ab5e9cee959b8b7e3
Adison01 <adisonsalauddin@gmail.com>
2024-10-30 10:57:50 -0500

    add all

--- backend/controllers/product.controller.js
+++ backend/controllers/product.controller.js
@@ -1,66 +1,82 @@
+import mongoose from "mongoose";
+import Product from "../models/product.model.js";
+import redis from "redis";
+import responseTime from "response-time";
+import { promisify } from "util";
+import { GET_ASYNC, SET_ASYNC } from "../server.js";
 
-import mongoose from 'mongoose';
-import Product from '../models/product.model.js'
 
 export const getProducts = async (req, res) => {
-    try {
-      const products = await Product.find({});
+  try {
+    const isProductInCache = await GET_ASYNC("/api/products");
+    if (isProductInCache) {
+      res.send(JSON.parse(isProductInCache));
       res.status(200).json({ success: true, data: products });
-    } catch (error) {
-      console.error("product not found: ", error.message);
-      res.status(500).json({ success: false, message: "server error" });
     }
-  };
+    const products = await Product.find({});
+    res.status(200).json({ success: true, data: products });
+    await SET_ASYNC("/api/products", JSON.stringify(products), 5);
+  } catch (error) {
+    console.error("product not found: ", error.message);
+    res.status(500).json({ success: false, message: "server error" });
+  }
+};
 
-  export const createProduct = async (req, res) => {
-    const product = req.body; // user will send this data
-    if (!product.name || !product.price || !product.image) {
-      return res
-        .status(400)
-        .json({ success: false, message: "Please provide all fields" });
-    }
-    const newProduct = new Product(product);
-    try {
-      await newProduct.save();
-      res.status(201).json({ success: true, data: newProduct });
-    } catch (error) {
-      console.error("Error in Create product: ", error.message);
-      res.status(500).json({ success: false, message: "Server Error" });
-    }
-  };
+export const createProduct = async (req, res) => {
+  const product = req.body; // user will send this data
+  if (!product.name || !product.price || !product.image) {
+    return res
+      .status(400)
+      .json({ success: false, message: "Please provide all fields" });
+  }
+  const newProduct = new Product(product);
+  try {
+    await newProduct.save();
+    // await SET_ASYNC("/api/products", JSON.stringify(newProduct), 5);
+    res.status(201).json({ success: true, data: newProduct });
+  } catch (error) {
+    console.error("Error in Create product: ", error.message);
+    res.status(500).json({ success: false, message: "Server Error" });
+  }
+};
 
-  export const updateProducts = async (req, res) => {
-    const { id } = req.params;
-    const product = req.body;
-  
-    if(!mongoose.Types.ObjectId.isValid(id)){
-      return res.status(404).json({success: false, message: "Invalid Product Id"})
-    }
-  
-    try {
-      const updatedProduct = await Product.findByIdAndUpdate(id, product, {
-        new: true,
-      });
-      res.status(200).json({ success: true, data: updatedProduct });
-    } catch (error) {
-      res.status(500).json({ success: false, message: "Server Error" });
-    }
-  };
+export const updateProducts = async (req, res) => {
+  const { id } = req.params;
+  const product = req.body;
 
-  export const deleteProduct = async (req, res) => {
-    //here colon id means it will be dynamic, any value we can pass
-    const { id } = req.params;
+  if (!mongoose.Types.ObjectId.isValid(id)) {
+    return res
+      .status(404)
+      .json({ success: false, message: "Invalid Product Id" });
+  }
 
-    if(!mongoose.Types.ObjectId.isValid(id)){
-      return res.status(404).json({success: false, message: "Invalid Product Id"})
-    };
+  try {
+    const updatedProduct = await Product.findByIdAndUpdate(id, product, {
+      new: true,
+    });
+    // await SET_ASYNC("/api/products", JSON.stringify(updatedProduct), 5);
+    res.status(200).json({ success: true, data: updatedProduct });
+  } catch (error) {
+    res.status(500).json({ success: false, message: "Server Error" });
+  }
+};
 
-    try {
-      await Product.findByIdAndDelete(id);
-      res.status(200).json({ success: true, message: "Product Deleted" });
-    } catch (error) {
-      res.status(500).json({ success: false, message: "Server error" });
-    };
-    
-    console.log("id : ", id);
-  };
+export const deleteProduct = async (req, res) => {
+  //here colon id means it will be dynamic, any value we can pass
+  const { id } = req.params;
+
+  if (!mongoose.Types.ObjectId.isValid(id)) {
+    return res
+      .status(404)
+      .json({ success: false, message: "Invalid Product Id" });
+  }
+
+  try {
+    await Product.findByIdAndDelete(id);
+    res.status(200).json({ success: true, message: "Product Deleted" });
+  } catch (error) {
+    res.status(500).json({ success: false, message: "Server error" });
+  }
+
+  console.log("id : ", id);
+};
--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,9 @@
 import express from "express";
 import dotenv from "dotenv";
 import path from "path";
+import redis from "redis"
+import responseTime from "response-time";
+import { promisify } from "util";
 
 import { connectDB } from "./config/db.js";
 
@@ -10,12 +13,23 @@ dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
+const client = redis.createClient({
+	host: "127.0.0.1",
+	port: 6379,
+});
+client.on('connect',  function(){
+	console.log("connec to redis")
+})
+
+export const GET_ASYNC = promisify(client.get).bind(client);
+export const SET_ASYNC = promisify(client.set).bind(client);
 
 const __dirname = path.resolve();
 
 app.use(express.json()); // allows us to accept JSON data in the req.body
 
 app.use("/api/products", productRoutes);
+app.use(responseTime());
 
 if (process.env.NODE_ENV === "production") {
 	app.use(express.static(path.join(__dirname, "/frontend/dist")));
